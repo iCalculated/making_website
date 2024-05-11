17148d4b472c004083b68094c7ad0211dd211c4f
iCalculated <fufa0001@gmail.com>
2024-05-11 12:27:08 -0500

    📓 scrapbook route

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,17 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/scrapbook",
+        destination:
+          "https://www.figma.com/file/5v5Y8XMrT9Ib0rvYgx7pSK/making%40umn-scrapbook?type=whiteboard&node-id=0-1&t=wbUKxpi9vYK8YhAS-0",
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
